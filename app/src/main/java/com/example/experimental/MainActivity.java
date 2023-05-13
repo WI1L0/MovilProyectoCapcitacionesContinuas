b9c3a9195829193aaf708420a5ef1805a75a91c1
byronn28 <byronpp16@gmail.com>
2023-05-12 21:36:24 -0500

    animcc pb

--- app/src/main/java/com/example/experimental/MainActivity.java
+++ app/src/main/java/com/example/experimental/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.experimental;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,8 +11,10 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 import com.example.experimental.DB.DataBase;
 import com.example.experimental.DB.DataBaseTemporal;
@@ -37,6 +41,15 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView3);
+
+        ObjectAnimator jumpAnimator = ObjectAnimator.ofFloat(imageView, "translationY", 0, 100, -100, 0);
+        jumpAnimator.setDuration(2000);
+        jumpAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        jumpAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        jumpAnimator.start();
+
 
         //use database
         conection1 = new DataBase(getApplicationContext());
